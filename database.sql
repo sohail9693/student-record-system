2ec39e14108514a3ac464d391f5f5fbb0c3567b6
sohail9693 <148769986+sohail9693@users.noreply.github.com>
2025-12-06 11:36:28 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,17 @@
+CREATE DATABASE student_db;
+
+USE student_db;
+
+CREATE TABLE students (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100),
+  email VARCHAR(100),
+  course VARCHAR(100),
+  marks INT
+);
+
+INSERT INTO students (name, email, course, marks)
+VALUES 
+("Rahul", "rahul@gmail.com", "CSE", 85),
+("Aditi", "aditi@gmail.com", "ECE", 78);
 
