ac2f138afc3a50cc2e253ebdeb6744c00d63089a
sohail9693 <148769986+sohail9693@users.noreply.github.com>
2025-12-06 11:35:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+let students = [
+  {id: 1, name: "Rahul", email: "rahul@gmail.com", course: "CSE", marks: 85},
+  {id: 2, name: "Aditi", email: "aditi@gmail.com", course: "ECE", marks: 78}
+];
+
+function loadTable() {
+  let output = "";
+  students.forEach(s => {
+    output += `
+      <tr>
+        <td>${s.id}</td>
+        <td>${s.name}</td>
+        <td>${s.email}</td>
+        <td>${s.course}</td>
+        <td>${s.marks}</td>
+      </tr>
+    `;
+  });
+
+  document.getElementById("data").innerHTML = output;
+}
+
+loadTable();
 
